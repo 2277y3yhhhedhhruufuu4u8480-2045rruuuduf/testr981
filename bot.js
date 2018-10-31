c391627b85e593af8b9e9f72ac10fc9c57825f3d
2277y3yhhhedhhruufuu4u8480-2045rruuuduf <44577021+2277y3yhhhedhhruufuu4u8480-2045rruuuduf@users.noreply.github.com>
2018-10-31 21:04:05 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('وعليكم السلام ورحمة الله وبركاتة');
+  }
+});
+
+
+const adminprefix = "!";
+const devs = ['311603535466856458','358664999914438658'];
+client.on('message', message => {
+ var argresult = message.content.split(` `).slice(1).join(' ');
+   if (!devs.includes(message.author.id)) return;
+   
+if (message.content.startsWith(adminprefix + 'setgame')) {
+ client.user.setGame(argresult);
+   message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else
+ if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+   message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+ if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+ message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+     } else    
+if (message.content.startsWith(adminprefix + 'setT')) {
+ client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوق دايموند كودز
+   message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//حقوق دايموند كودز
+}
+});
+ 
+client.login("NTA2ODQ5NDMxNDg5MDg1NDYx.DroIDA.my6ymVXH-ModEWWrk91la14-5dI");
