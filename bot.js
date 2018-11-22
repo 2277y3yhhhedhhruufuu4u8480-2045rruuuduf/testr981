99716adad56a6afd23241351ceef9a0e833694c8
2277y3yhhhedhhruufuu4u8480-2045rruuuduf <44577021+2277y3yhhhedhhruufuu4u8480-2045rruuuduf@users.noreply.github.com>
2018-11-22 18:31:12 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,40 +1,41 @@
-const Discord = require('discord.js');
+
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuute,");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'السلام عليكم') {
-    msg.reply('وعليكم السلام ورحمة الله وبركاتة');
-  }
-});
 
 
+
+ const devs = ['466914661670060032' , '' , ''];
 const adminprefix = "!";
-const devs = ['490891548494659586','490891548494659586'];
 client.on('message', message => {
- var argresult = message.content.split(` `).slice(1).join(' ');
-   if (!devs.includes(message.author.id)) return;
-   
-if (message.content.startsWith(adminprefix + 'setgame')) {
- client.user.setGame(argresult);
-   message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
- if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-   message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
- if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
- message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-     } else    
-if (message.content.startsWith(adminprefix + 'streaming')) {
- client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوق دايموند كودز
-   message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//حقوق دايموند كودز
-}
-});
- 
-client.login("NTA3MjQ1NjA3MDMxNjY4Nzc2.DruARA.uc2ZA7MUdwi4L8hG6ZZYQI--RR0");
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} ply .. :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} wt .. :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} ls .. :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+      message.channel.sendMessage(`**  ${argresult} st .. :white_check_mark:**`)
+    }
+  if (message.content.startsWith(adminprefix + 'us')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**us ..**${argresult}** `)
+  } else
+  if (message.content.startsWith(adminprefix + 'av')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**avatar ... :** `);
+  }
+  });
+client.login("توكن")
